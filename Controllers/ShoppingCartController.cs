9dda7892d261696f36919cfedce13213af471c15
mallet1 <smallet079@gmail.com>
2024-04-04 23:09:56 -0500

    add transaction history and shopping cart

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,21 @@
+﻿using iCLOTHINGWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iCLOTHINGWebsite.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private iCLOTHINGEntities db = new iCLOTHINGEntities();
+
+        // GET: ShoppingCart
+        public ActionResult Index()
+        {
+            var data = db.SHOPPINGCART.Include("ITEM").Include("USERS");
+            return View(data.ToList());
+        }
+    }
+}
